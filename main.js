dff2bcde992669ed5b2ab7aad4b304f41e7aff76
Jahaan-boop <85781919+Jahaan-boop@users.noreply.github.com>
2022-11-22 08:21:36 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 song1 = "";
 song2 = "";
+peterpan = "";
+harrypotter = "";
 lx=0;
 ly=0;
+ls=0;
 rx=0;
 ry=0;
+rs=0;
 
 function preload(){
 song1=loadSound("music.mp3");
@@ -21,6 +25,34 @@ posenet.on("pose", gotposes);
 
     function draw(){
         image(video, 0,0,600,500)
+
+        peterpan=song2.isPlaying();
+        console.log(peterpan);
+
+        fill("red")
+        stroke("black")
+
+
+        if(ls > 0.2){
+        circle(lx,ly,20);
+        song1.stop();
+        if(song2 == false){
+        song2.play();
+        document.getElementById("song").innerHTML="Song Name: Peter Pan Theme Song";
+        }
+        }
+
+       harrypotter=song1.isPlaying();
+        console.log(harrypotter);
+
+        if(rs > 0.2){
+        circle(rx,ry,20);
+        song2.stop();
+        if(song1 == false){
+        song1.play();
+        document.getElementById("song").innerHTML="Song Name: Harry Potter Theme Song";
+        }
+        }
         }
 
         function modelloaded(){
@@ -31,12 +63,15 @@ posenet.on("pose", gotposes);
                 if(results.length>0){
                 console.log(results);
                 lx=results[0].pose.leftWrist.x;
-                rx=results[0].pose.rightWrist.x;
                 ly=results[0].pose.leftWrist.y;
+                ls=results[0].pose.keypoints[9].score;
+                rx=results[0].pose.rightWrist.x;
                 ry=results[0].pose.rightWrist.y;
+                rs=results[0].pose.keypoints[10].score;
                 console.log("leftwristx="+lx);
-                console.log("rightwristx="+rx);
                 console.log("leftwristy="+ly);
+                console.log("leftwristscore="+ls);
+                console.log("rightwristx="+rx);
                 console.log("rightwristx="+ry);
                 }
                 }
